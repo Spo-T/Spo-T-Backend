1c9f209db21608a78d1ebd70be2c4025433abaf5
woonseok105 <yws3786@gmail.com>
2024-02-22 16:31:01 +0900

    current user decorator

---
+++ src/infrastructure/domain/decorator/current-user.ts
@@ -0,0 +1,7 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { UserEntity } from '../user/persistence/user.entity';
+
+export const CurrentUser = createParamDecorator((data, ctx: ExecutionContext): UserEntity => {
+    const req = ctx.switchToHttp().getRequest();
+    return req.user;
+});
