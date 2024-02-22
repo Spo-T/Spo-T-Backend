30d2f7751a16f2d13ad9505771f993ac865a3c90
woonseok105 <yws3786@gmail.com>
2024-02-22 16:58:32 +0900

    modify dir

--- src/infrastructure/global/decorator/current-user.ts
+++ src/infrastructure/global/decorator/current-user.ts
@@ -1,5 +1,5 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { UserEntity } from '../user/persistence/user.entity';
+import { UserEntity } from '../../domain/user/persistence/user.entity';
 
 export const CurrentUser = createParamDecorator((data, ctx: ExecutionContext): UserEntity => {
     const req = ctx.switchToHttp().getRequest();
